a926543c910ba90f320873f7038cd8fb50d6362e
komishs <komishs@github.com>
2024-01-31 13:16:57 +0900

    init.html commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World(modified)"}
+# directory : 폴더 지정
+templates = Jinja2Templates(directory="templates")
+
+# @app.get("/")
+@app.get("/", response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
+
+@app.get("/hello")
+async def hello():
+    return "(fastapi) 안녕! 반가워!"
+
